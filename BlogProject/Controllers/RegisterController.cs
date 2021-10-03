2571564eacedd06987c25941d90805a51624687b
Muharrem Sait ÇOKTAŞ <coktasmuharremsait@gmail.com>
2021-10-03 20:59:14 +0300

    Add Writer Service and Manager, Register: Add controller, view model, view for this

---
+++ BlogProject/Controllers/RegisterController.cs
@@ -0,0 +1,43 @@
+﻿using BlogProject.Models;
+using BusinessLayer.Concrete;
+using DataAccessLayer.Concrete.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlogProject.Controllers
+{
+    public class RegisterController : Controller
+    {
+        //Register/Index
+
+        WriterManager writerManager = new WriterManager(new EfWriterRepository());
+
+        //Ekleme islemi yapilirken, HttpGet ve HttpPost attributelerinin 
+        //tanimlandigi methodlarin isimleri ayni olmak zorunda
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(Writer p, RegisterViewModel registerViewModel)
+        {
+            if(!ModelState.IsValid)
+            {
+                return View();
+            }
+
+            p.Status = true;
+            p.About = "Test";
+            writerManager.WriterAdd(p);
+            //Redirect to main page after the action
+            return RedirectToAction("Index", "Blogs");
+        }
+    }
+}
---
+++ BlogProject/Models/RegisterViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlogProject.Models
+{
+    public class RegisterViewModel
+    {
+        [Required(ErrorMessage = "Password is required.")]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "Confirmation Password is required.")]
+        [Compare("Password", ErrorMessage = "Password and Confirmation Password must match.")]
+        public string ConfirmPassword { get; set; }
+    }
+}
