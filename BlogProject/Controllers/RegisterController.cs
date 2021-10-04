a66f018fd674df9919d1e912f73acba0e95b5ccf
Muharrem Sait ÇOKTAŞ <coktasmuharremsait@gmail.com>
2021-10-04 18:10:31 +0300

    Add Fluent Validation

--- BlogProject/Controllers/RegisterController.cs
+++ BlogProject/Controllers/RegisterController.cs
@@ -1,7 +1,9 @@
 ﻿using BlogProject.Models;
 using BusinessLayer.Concrete;
+using BusinessLayer.ValidationRules;
 using DataAccessLayer.Concrete.EntityFramework;
 using EntityLayer.Concrete;
+using FluentValidation.Results;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -25,19 +27,44 @@ namespace BlogProject.Controllers
             return View();
         }
 
+        //RegisterViewModel içerisinde ConfirmPassword ve Password alanlari için validation var.
+        //Formdaki bilgiler(ConfirmPassword ve Password input degerleri bu alanlara map ediliyor.
+        //Validation bu degerler icin calisiyor ve eger validation gecersiz olursa
+        //ViewModelState.IsValid = False oluyor.Index.cshtml'de bu map islemi icin
+        //Ve validation gecersizlik message(ErrorMessage) yi o fieldin hemen altinda 
+        //gostermek icin TagHelpers kullandik.Boylecek is valid false oldugunda
+        //View tekrar yuklenir ama bu sefer ErrorMessageleri de gosterir
+
         [HttpPost]
         public IActionResult Index(Writer p, RegisterViewModel registerViewModel)
         {
+            //Confirm Passsword
             if(!ModelState.IsValid)
             {
                 return View();
             }
 
-            p.Status = true;
-            p.About = "Test";
-            writerManager.WriterAdd(p);
-            //Redirect to main page after the action
-            return RedirectToAction("Index", "Blogs");
+            //Validate Writer
+            WriterValidator writerValidator = new WriterValidator();
+            //Hangi degerleri validate edeceksin,
+            //parametre olarak gonderilen Writeri validate edeceksin
+            ValidationResult results = writerValidator.Validate(p);
+            if (results.IsValid) //eger sonuclar gecerliyse
+            {
+                p.Status = true;
+                p.About = "Test";
+                writerManager.WriterAdd(p);
+                //Redirect to main page after the action
+                return RedirectToAction("Index", "Blogs");
+            }
+            else
+            {
+                foreach (var item in results.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+            }
+            return View();
         }
     }
 }
---
+++ BusinessLayer/ValidationRules/WriterValidator.cs
@@ -0,0 +1,34 @@
+﻿using EntityLayer.Concrete;
+using FluentValidation;
+
+namespace BusinessLayer.ValidationRules
+{
+    //HEY! You are a validator and you should be validate the writer entity
+    public class WriterValidator : AbstractValidator<Writer>
+    {
+        public WriterValidator()
+        {
+            //Validation Rules For Writer Entity
+            RuleFor(x => x.FullName).NotEmpty().WithMessage(
+                "Author name and surname cannot be blank!");
+
+            RuleFor(x => x.Email).EmailAddress().NotEmpty().WithMessage(
+                "Email field cannot be blank!");
+
+            RuleFor(x => x.WriterImage).NotEmpty().WithMessage(
+                "Image field cannot be blank!");
+
+            RuleFor(x => x.FullName).MinimumLength(2).WithMessage(
+                "Full Name must be at least 2 characters");
+
+            RuleFor(x => x.FullName).MaximumLength(50).WithMessage(
+                "Full Name can be up to 50 characters");
+
+            RuleFor(x => x.Password).NotEmpty().WithMessage(
+                "Password field cannot be blank!");
+
+            RuleFor(x => x.Password).Matches(@"[0-9]+[A-Z]+[a-z]").WithMessage(
+                "Password must contain at least one lowercase letter, one uppercase letter, and one number"); ;
+        }
+    }
+}
