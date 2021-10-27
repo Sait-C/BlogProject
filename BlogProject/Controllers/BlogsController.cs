aabfede09045d4dcd622d4b512afa7b97ae9b3d2
Muharrem Sait ÇOKTAŞ <coktasmuharremsait@gmail.com>
2021-10-27 18:59:25 +0300

    Add BlogAdd, BlogListByWriter and Blog Validator

--- BlogProject/Controllers/BlogsController.cs
+++ BlogProject/Controllers/BlogsController.cs
@@ -1,7 +1,11 @@
 ﻿using BusinessLayer.Concrete;
+using BusinessLayer.ValidationRules;
 using DataAccessLayer.Concrete.EntityFramework;
+using EntityLayer.Concrete;
+using FluentValidation.Results;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -28,6 +32,53 @@ namespace BlogProject.Controllers
             ViewBag.i = id;
             var values = blogManager.GetBlogById(id);
             return View(values);
+        }
+
+        public IActionResult BlogListByWriter()
+        {
+            var values = blogManager.GetAllByWriter(1);
+            return View(values);
+        }
+
+        [HttpGet]
+        public IActionResult BlogAdd()
+        {
+            CategoryManager categoryManager = new CategoryManager(new EfCategoryRepository());
+            List<SelectListItem> category_values = (from x in categoryManager.GetAll()
+                                                    select new SelectListItem
+                                                    {
+                                                        //benim burada amacim:
+                                                        //bloglari eklerken, bloglari sectigim zaman id degerinin gonderilmesi
+                                                        //dropdown aracinda
+                                                        Text = x.CategoryName,
+                                                        Value = x.CategoryID.ToString()
+                                                    }).ToList();
+            ViewBag.categoryvalues = category_values;
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult BlogAdd(Blog blog)
+        {
+            BlogValidator blogValidator = new BlogValidator();
+            ValidationResult results = blogValidator.Validate(blog);
+            if (results.IsValid)
+            {
+                blog.Status = true;
+                blog.CreatedDate = DateTime.Parse(DateTime.Now.ToShortDateString());
+                blog.WriterId = 1;
+                blogManager.Add(blog);
+
+                return RedirectToAction("BlogListByWriter", "Blogs");
+            }
+            else
+            {
+                foreach (var item in results.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+            }
+            return View();
         }
     }
 }
---
+++ BusinessLayer/ValidationRules/BlogValidator.cs
@@ -0,0 +1,24 @@
+﻿using EntityLayer.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.ValidationRules
+{
+    public class BlogValidator : AbstractValidator<Blog>
+    {
+        public BlogValidator()
+        {
+            RuleFor(x => x.Title).NotEmpty().WithMessage("Blog Title cannot be blank!");
+            RuleFor(x => x.Content).NotEmpty().WithMessage("Blog Content cannot be blank!");
+            RuleFor(x => x.BlogImage).NotEmpty().WithMessage("Blog Image cannot be blank!");
+            RuleFor(x => x.BlogImage).NotEmpty().WithMessage("Blog Image cannot be blank!");
+            RuleFor(x => x.Title).MaximumLength(150).WithMessage("The blog title must be less than 150 characters!");
+            RuleFor(x => x.Title).MinimumLength(5).WithMessage("The blog title must be more than 5 characters!");
+            RuleFor(x => x.Content).MinimumLength(50).WithMessage("The blog content must be more than 5 characters!");
+        }
+    }
+}
